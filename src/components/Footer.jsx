a2de9d86f0f6e2d3c681ab293c3fd103202d3a12
Ivan <nenad.novak80@gmail.com>
2021-03-10 14:21:12 +0100

    'hhhhh'

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div style={{fontSize:'30px'}} className='footer'>
+            <h1>footer</h1>
+            <p>hgkmfgkfhf</p>
+        </div>
+    )
+}
+
+export default Footer
